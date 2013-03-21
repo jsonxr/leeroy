824d0cc762d629a34069214891900a30c1885949
jasonxrowland <jasonxrowland@gmail.com>
2013-03-20 23:25:06 -0600

    shrug

--- controllers/play.py
+++ controllers/play.py
@@ -1,2 +1,15 @@
+from subprocess import Popen
+from subprocess import call
+
 def mp3():
+    filename = request.vars['filename']
+    volume = request.vars['volume']
+    Popen(["mpg321", "/home/pi/web2py/applications/leeroy/static/mp3/" + filename])
     return "Yay"
+    
+def volume():
+    v = request.vars['v']
+    call(["amixer", "cset", "numid=1", "--", v + "%"])
+    Popen(["mpg321", "/home/pi/leeroy.mp3"])
+    return "done"
+     
